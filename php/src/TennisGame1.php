b4cdf7f9885160e64d4b984f7689b2955661b4ee
Sergio Osta <sergio.osta@biko2.com>
2021-02-26 09:03:47 +0100

    Primera refactorización

--- php/src/TennisGame1.php
+++ php/src/TennisGame1.php
@@ -2,6 +2,8 @@
 
 namespace TennisGame;
 
+use phpDocumentor\Reflection\Types\Boolean;
+
 class TennisGame1 implements TennisGame
 {
     private $m_score1 = 0;
@@ -26,57 +28,64 @@ class TennisGame1 implements TennisGame
 
     public function getScore()
     {
-        $score = "";
-        if ($this->m_score1 == $this->m_score2) {
-            switch ($this->m_score1) {
-                case 0:
-                    $score = "Love-All";
-                    break;
-                case 1:
-                    $score = "Fifteen-All";
-                    break;
-                case 2:
-                    $score = "Thirty-All";
-                    break;
-                default:
-                    $score = "Deuce";
-                    break;
-            }
-        } elseif ($this->m_score1 >= 4 || $this->m_score2 >= 4) {
-            $minusResult = $this->m_score1 - $this->m_score2;
-            if ($minusResult == 1) {
-                $score = "Advantage player1";
-            } elseif ($minusResult == -1) {
-                $score = "Advantage player2";
-            } elseif ($minusResult >= 2) {
-                $score = "Win for player1";
-            } else {
-                $score = "Win for player2";
-            }
-        } else {
-            for ($i = 1; $i < 3; $i++) {
-                if ($i == 1) {
-                    $tempScore = $this->m_score1;
-                } else {
-                    $score .= "-";
-                    $tempScore = $this->m_score2;
-                }
-                switch ($tempScore) {
-                    case 0:
-                        $score .= "Love";
-                        break;
-                    case 1:
-                        $score .= "Fifteen";
-                        break;
-                    case 2:
-                        $score .= "Thirty";
-                        break;
-                    case 3:
-                        $score .= "Forty";
-                        break;
-                }
-            }
+        if ($this->isEquales()) {
+            return $this->getEqualesScore();
         }
+        $score = $this->getGameScore($this->m_score1,$this->m_score2);
+
         return $score;
+    }
+    private function isEquales() {
+        if ($this->m_score1 == $this->m_score2) {
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    private function getEqualesScore() {
+        $posiblesScores  = [
+            0 => 'Love-All',
+            1 => 'Fifteen-All',
+            2 => 'Thirty-All',
+        ];
+        return array_key_exists($this->m_score2,$posiblesScores) ? $posiblesScores[$this->m_score2] : 'Deuce';
+    }
+
+
+    private function getGameScore($m_score1, $m_score2) {
+        if ($this->m_score1 >= 4 && $this->m_score2 >= 4) {
+            $minusResult = $this->m_score1 - $this->m_score2;
+            return $this->getAdventageScore($minusResult);
+        }
+        $posiblesScores  = [
+            0 => 'Love',
+            1 => 'Fifteen',
+            2 => 'Thirty',
+            3 => 'Forty'
+        ];
+        if($m_score1 == 4) {
+            if ($m_score2 == 3) {
+                return 'Advantage player1';
+            }
+            return 'Win for player1';
+        }
+        if($m_score2 == 4) {
+            if ($m_score1 == 3) {
+                return 'Advantage player2';
+            }
+            return 'Win for player2';
+        }
+        return $posiblesScores[$m_score1] . "-" . $posiblesScores[$m_score2];
+    }
+
+    private function getAdventageScore($minusResult) {
+        $posiblesScores  = [
+            -1 => 'Advantage player2',
+            1 => 'Advantage player1',
+            2 => 'Win for player1',
+            -2 => 'Win for player2'
+        ];
+        return $posiblesScores[$minusResult];
+
     }
 }
